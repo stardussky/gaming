008365579e406af3e26e23ea5d2b3df1cfedfff6
Evan <stardussky@gmail.com>
2022-12-26 23:34:38 +0800

    部署測試

---
+++ sync.sh
@@ -0,0 +1,1 @@
+AzCopy sync "dist" "https://shlmedicalgame.blob.core.windows.net/\$web?sv=2019-12-12&ss=btqf&srt=sco&st=2022-12-23T15%3A37%3A11Z&se=2023-12-24T15%3A37%3A00Z&sp=rwdxftlacup&sig=AGAzyYMjw2s51rXwy1G9glk%2Fo7FpGj7UyAFX8iSy7ME%3D" --delete-destination "true"
